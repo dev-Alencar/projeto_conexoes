3bb59527eb029e2fb9d02b4b76dba4cd9dd7ece0
Luc Alencar <lucalencar.dev@gmail.com>
2023-11-14 16:23:21 -0300

    ajuste de estilo

--- project-conection/src/Menu.jsx
+++ project-conection/src/Menu.jsx
@@ -1,16 +1,19 @@
 import './menu.css'
 function Menu () {
     return(
-    <div className='teste'>
-        <div className='big-box'>
-                <nav className='into-box'>
-                    <a  className='links-menu' href="/"> Home </a> <br/> <br/>
-                    <a className='links-menu' href="/sobre"> Sobre</a> <br/> <br/>
-                    <a className='links-menu' href="/contato"> Contato </a> <br/> <br/>
-                    <a className='links-menu' href="/noticias"> Notícias </a> <br/> <br/>
-                </nav>
-        </div>
+    <div>
+        <div className='menu-center'>
+            <div className='big-box'>
+                    <nav className='into-box'>
+                        <a  className='links-menu' href="/"> Home </a> <br/> <br/>
+                        <a className='links-menu' href="/sobre"> Sobre</a> <br/> <br/>
+                        <a className='links-menu' href="/contato"> Contato </a> <br/> <br/>
+                        <a className='links-menu' href="/noticias"> Notícias </a> <br/> <br/>
+                    </nav>
+            </div>
     </div>
+    </div>   
+    
      
     )
 }
