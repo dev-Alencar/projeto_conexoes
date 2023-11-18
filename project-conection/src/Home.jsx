2b9a89b12a7bdda4c8f838b08a2dbaa3a33b0c9d
Luc Alencar <lucalencar.dev@gmail.com>
2023-11-18 16:49:42 -0300

    luscas

--- project-conection/src/Home.jsx
+++ project-conection/src/Home.jsx
@@ -1,4 +1,5 @@
 import Menu from "./Menu"
+import { Link } from "react-router-dom"
 
 import './home.css'
 
@@ -11,7 +12,7 @@ function Home () {
     return(
         <div>
             <Menu/>
-            <div className='teste(grid-img)'>
+            <div className='teste'>
                 <img className='danca-img' src={danca}/>
                 <img className='image1' src={image1}/>
                 <div className='teste2'>
@@ -21,10 +22,13 @@ function Home () {
                 <img className='image4' src={image4}/>
                 </div>
             </div>
-            
+
          <div className='box-about'>
                 <h1 className='title'> SOBRE </h1>
-                <p className='text'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p className='text'>
+                Bem-vindo ao site da Sejuv Recife, a Secretaria Executiva da Juventude que tem como missão principal promover e fortalecer a participação ativa dos jovens na construção de uma cidade mais inclusiva e dinâmica. 
+                Aqui você irá encontrar datas dos próximos eventos, formulários para inscrições, editais e outras informações como nossas redes sociais e canais de contato.
+                </p>
          </div>
 
 
@@ -34,36 +38,37 @@ function Home () {
          <h1 className='title'>CATEGORIAS</h1>
                 <div>
                     <button className="button-event">
-                        <p className='text-buttons'>EVENTOS</p>
+                        <Link className='text-buttons' target="_blank" to="/"> EVENTOS </Link> <br/> <br/>
                     </button>
                     <button className="button-event second-button">
-                        <p className='text-buttons'>EVENTOS</p>
+                    <Link className='text-buttons' target="_blank" to="/"> INFORMAÇÕES </Link>
                     </button>
                 </div>
 
                 <div>
                     <button className="button-event">
-                        <p className='text-buttons'>EVENTOS</p>
+                    <Link className='text-buttons' target="_blank" to="/"> EDITAIS </Link> <br/> <br/>
+                        
                     </button>
                     <button className="button-event second-button">
-                        <p className='text-buttons'>EVENTOS</p>
+                    <Link className='text-buttons' target="_blank" to="/"> ID JOVEM </Link>
                     </button>
                 </div>
 
                 <div>
                     <button className="button-event">
-                        <p className='text-buttons'>EVENTOS</p>
+                    <Link className='text-buttons' target="_blank" to="/"> FORMULÁRIOS </Link>
                     </button>
                     <button className="button-event last-button">
-                        <p className='text-buttons'>EVENTOS</p>
+                    <Link className='text-buttons' target="_blank" to="/"> DENÚNCIAS </Link>
                     </button>
                 </div>
         <div className='draft-box'>
         <img className='draft-img' src={destaque}/>
         </div>
-        </div> 
+        </div>
     </div>
-        
+
     )
 }
 
